9ef25ad3d21d500798516ca0b49b7e401a007b83
Kristian Rune Larsen <krl@letsgo.dk>
2015-02-23 00:24:52 +0100

    working small argparse script

--- intermediate_python/dag_1/005_small_script.py
+++ intermediate_python/dag_1/005_small_script.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python
 # coding : utf-8
+#
+#
+# small argparse example 
+# run with 
+# python 005_small_script.py 4 --directory=/tmp/
+#
+# or 
+#
+# python 005_small_script.py
+#
+# to see argparse options
 
 import os
 import sys
 import argparse
 
 
-parser = argparse.ArgumentParser(description='list n files in directory')
-parser.add_argument('files',
-                    metavar='N',
+parser = argparse.ArgumentParser(description='list N files in directory')
+parser.add_argument('N',
                     type=int,
-                    nargs='+',
                     help='How many files should be listed')
 
 parser.add_argument('--directory',
-                    dest='directory',
-                    action='store_const',
-                    type=str,
                     default='.',
                     help='directory to list')
 
 
+args = parser.parse_args()
+
+files = os.listdir(args.directory)
+for fn in files[:min(args.N, len(files))]:
+    print(fn)
 
 
